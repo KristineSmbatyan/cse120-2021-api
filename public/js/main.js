858144a1136f8dd47605c80157d092613767ccdf
KristineSmbatyan <81805696+KristineSmbatyan@users.noreply.github.com>
2021-05-09 11:30:21 +0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -240,25 +240,7 @@ function displayData(data, containerDivName) {
   updatedHobby.Q5 = document.getElementById("Q5").value;
   updatedHobby.Q6 = document.getElementById("Q6").value;
   updatedHobby.Q7 = document.getElementById("Q7").value;
-  
- $.ajax({
-      type: 'POST',
-      url: "https://cse120-2021-kristine.herokuapp.com/data/update",
-      data: updatedHobby,
-      cache: false,
-      dataType : 'json',
-      success: function (data) {
-        console.log("success");
-      },
-      error: function (xhr) {
-        console.error("Error in post", xhr);
-      },
-      complete: function () {
-        console.log("Complete");  
-      }
-    });
-  
-}
+ 
 function updateBookDataChenges(e) {
   e.preventDefault();
   var updatedBook = {};
