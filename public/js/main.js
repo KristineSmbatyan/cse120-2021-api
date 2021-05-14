64d97b5f16376f97ad381e96ed212a60497fb8e1
KristineSmbatyan <81805696+KristineSmbatyan@users.noreply.github.com>
2021-05-14 23:11:41 +0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -218,25 +218,6 @@ function editData(id) {
   }
 
 
-    document.querySelectorAll("#chessDataContainer div.item").forEach(div => {
-          div.addEventListener("click", function(e){
-          if (this.style.height == "auto") {
-            this.style.height = "30px";
-          } else {
-            this.style.height = "auto";
-          }
-          })        
-    })
-    
-    document.querySelectorAll("#bookDataContainer div.item").forEach(div => {
-          div.addEventListener("click", function(e){
-          if (this.style.height == "auto") {
-              this.style.height = "30px";
-            } else {
-              this.style.height = "auto";
-            }
-            })        
-    })
 
 
 
